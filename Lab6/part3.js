2cb9000efa89fad08fc97df424dcd6bd9ead7f26
Malak Nasser <malaknasser26@gmail.com>
2024-01-12 15:40:19 +0200

    Add lab 6

--- Lab6/part3.js
+++ Lab6/part3.js
@@ -23,6 +23,6 @@ console.log(Triple.description); // 'I triple any number you provide'
 console.log(Triple.calculate()); // 3
 console.log(Triple.calculate(6)); // 18
 console.log(SquaredTriple.calculate(3)); // 81 (not affected by parent's instantiation)
-console.log(SquaredTriple.description); // 'I square the triple of any number you provide'
+console.log(SquaredTriple.description); // 'I'm square the triple of any number you provide'
 console.log(SquaredTriple.longDescription); // undefined
 console.log(SquaredTriple.customName); //Tripler
